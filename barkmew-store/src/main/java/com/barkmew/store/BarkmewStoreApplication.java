bf7c707f883cfaf7877c7fcad62ee9aac1f343c1
LauraG-M <lalugoma9@gmail.com>
2024-06-19 17:10:39 -0500

    Primer commit

---
+++ barkmew-store/src/main/java/com/barkmew/store/BarkmewStoreApplication.java
@@ -0,0 +1,13 @@
+package com.barkmew.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkmewStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkmewStoreApplication.class, args);
+	}
+
+}
